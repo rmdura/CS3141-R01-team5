669636535b278d809e6c5439d46690393e308936
Sean Leverenz <snlevere@mtu.edu>
2022-03-27 21:16:49 -0400

    Attempted to make the pop up for sign up
    -Moved php code from SignUp.php to index.php
    -Created pop up for sign up in index.html
    -Added a few classes to the style.css

--- index.php
+++ index.php
@@ -27,7 +27,7 @@
     <p>
         The platform where college students can get together and have a good time! Use this website to join and communicate events <br>and activities around campus to your fellow peers. This tool is a great and easy way to get out and meet new people!
     </p>
-    <a href="Signup.php" class="signup-btn">Sign Up Now</a>
+    <button onclick="document.getElementById('id02').style.display='block'" class="signup-btn">Sign Up Now</button>
 </div>
     </section>
     <section class="activities">
@@ -93,7 +93,7 @@
         </div>
     </section>
 
-<!-- Start of database connection and editing --> 
+<!-- Start of database connection and editing for login --> 
 <?php
 if (isset($_POST["testUsername"]) && isset($_POST['testPassword'])) 
 {
@@ -164,7 +164,7 @@ if (isset($_POST["testUsername"]) && isset($_POST['testPassword']))
                         </div>
                     </div>
                     <div class="sign-up">
-                        <a href="Signup.php">New to College Socializing Platform?</a>
+                        <button onclick="document.getElementById('id02').style.display='block'" class="signin-btn">New to College Socializing Platform?</button>
                     </div>
 
                     <button type="button" onclick="document.getElementById('id01').style.display='none'" class="cancel-btn">Cancel</button>
@@ -183,6 +183,155 @@ if (isset($_POST["testUsername"]) && isset($_POST['testPassword']))
                 }
             }
         </script>
+    </section>
+
+<!-- Start of database connection and editing for signup --> 
+<?php
+if (isset($_POST["newUsername"]) && isset($_POST['newPassword']) && isset($_POST['newBirthdate']) && isset($_POST['newEmail'])) 
+{
+	$username = $_POST["newUsername"]; // variable username is the value in the username text box
+	if ($username == ""){ //check if username has been entered
+		?>
+		<script> alert ("Please enter username.")
+		window.location.href='index.php';
+		</script>
+		<?php
+	}
+	else{
+	setcookie("username",$username); // set cookie so username can be used later
+	$email = $_POST['newEmail'];
+	if ($email == ""){ //check if email has been entered
+		?>
+		<script> alert ("Please enter email.")
+		window.location.href='index.php';
+		</script>
+		<?php
+	}
+	else{
+	if (strpos($email, '@') == false)
+	{//email address given is not valid (doesn't contain @)
+		?>
+		<script> alert ("Must use a valid email address.")</script>
+		<?php
+	}
+	else{
+
+	list($user, $domain) = explode('@', $email); //split email into username and domain
+	if ($domain != 'mtu.edu') //if the domain is not an mtu.edu email, the user will be given a popup warning, which will redirect to signup page when exited out of
+	{
+		?>
+		<script> alert ("Must use a valid MTU address.")</script>
+		<?php
+	}
+	else{ //valid email, can move on
+	$password = $_POST['newPassword'];
+	if ($password == ""){ //check if password has been entered
+		?>
+		<script> alert ("Please enter password.")
+		window.location.href='index.php';
+		</script>
+		<?php
+	}
+	else{
+	$time = strtotime($_POST['newBirthdate']); //convert date in html form to sql date format
+	$birthdate = date('Y-m-d', $time);
+	if ($newBirthdate == ""){ //check if birthdate has been entered
+		?>
+		<script> alert ("Please enter birthdate.")
+		window.location.href='index.php';
+		</script>
+		<?php
+	}
+	else{
+	try{
+		$config = parse_ini_file("ProjectDB.ini"); // find database info in .ini file
+ 		$dbh = new PDO($config['dsn'], $config['username'], $config['password']); // create connection to database
+  		$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); // error checking
+		
+		//test to see if username or email are already in use
+		$userResult = $dbh->query("select * from Student where username = '" .$username ."'");
+		$userFound = $userResult->fetch();
+		$emailResult = $dbh->query("select * from Student where email = '" .$email ."'");
+		$emailFound = $emailResult->fetch();
+		if ($userFound) //username is already in use, popup error will be given
+		{
+			?>
+			<script> alert ("This username is already in use.  Please choose a new username.")</script>
+			<?php
+		}
+		elseif ($emailFound) //email is already in use, popup error will be given
+		{
+			?>
+			<script> alert ("There is already an account under this email.  If you have forgotten your password, please return to the home page to reset it.")</script>
+			<?php
+
+		}
+		else{ //username and email are new, good to continue
+		$query = "Insert into Student(username, email, password, birthdate) values(:newUser, :newEmail, :newPass, :newBirthdate)"; //query to create new account
+		$step = $dbh->prepare($query); //prepare statement to prevent SQL injection
+		$step->bindParam(':newUser', $username); //bind the parameters of query to form answers
+		$step->bindParam(':newEmail', $email);
+		$step->bindParam(':newPass', $password);
+		$step->bindParam(':newBirthdate', $birthdate);
+
+		if($step->execute()){
+			//If query was successful, shows pop up and then redirected to index.php
+			?>
+			<script> alert ("Account created successfully!")
+			window.location.href='index.php';
+			</script>
+			<?php
+		}
+		else{
+			//print error messages if query could not be performed
+			print "The account was not able to be created.  Please try again.";
+			print $e->getMessage();	
+		}
+		}	
+		
+	
+	} catch (PDOException $e) {
+		//print error messages if query could not be performed due to backend/database issues
+		print "The account was not able to be created.  Please try again.";
+		print $e->getMessage();
+		die();
+	}
+	}
+	}}}}}
+}
+?>
+
+    <!-- Signup Button Section -->
+    <section>
+        <div id="id02" class="modal">
+            <div class="login-div">
+                <div class="title">Welcome to College Socializing Platform</div>
+
+                <div class="form">
+                    <form method = "post" >
+                        <div class="newUsername">
+                            <input type="text" placeholder="Username">
+                        </div>
+
+                        <div class="newEmail">
+                            <input type="text" placeholder="Email">
+                        </div>
+
+                        <div class="newBirthdate">
+                            <input placeholder = "Birthdate" class = "textbox-n" type = "text" onfocus = "(this.type = 'date')"  id = "date">
+                        </div>
+
+                        <div class="newPassword">
+                            <input type="password" placeholder="Password">
+                        </div>
+
+                        <input type = "submit" name = "ok" value = "Create Account" class ="signin-btn">
+                    </form>
+
+                    <button type="button" onclick="document.getElementById('id02').style.display='none'" class="cancel-btn">Cancel</button>
+                </div>
+            </div>
+        </div>
     </section>
 </body>
 </html>
