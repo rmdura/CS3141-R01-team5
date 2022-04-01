b45aefdcd4f372fe59247c0fb01179638e96f53a
Scorekeeper2883 <pjellens@mtu.edu>
2022-04-01 14:48:48 -0400

    Update DashBoard.php
    Added head to file and php that should eventually check for a connection to server.

--- DashBoard.php
+++ DashBoard.php
@@ -1,5 +1,18 @@
 <html>
+<head>
+    <meta name="viewport" content="with=device-width, initial-scale=1.0">
+    <title>MTU Student Socializing Platform</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;300;400;600;700&display=swap" rel="stylesheet">
+</head>
 <body>
- <p>Successful login.</p>
+		<?php
+		require_once('connection.php');
+		$result = $conn->prepare("SELECT * FROM Student ORDER BY username ASC");
+		$result->execute();
+		?>
+	<p>Successful login.</p>
 </body>
 </html>
