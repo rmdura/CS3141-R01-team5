0d89ecb063ff4b6284a9334d16490a67cffd856d
rmdura <rmdura@mtu.edu>
2022-04-02 17:00:28 -0400

    Allow for signup directly from index.php
    Got signup working with php in index.php file.  Will work on putting it into a seperate file that can be brought in with an include statement.  Also indented and organized php

--- index.php
+++ index.php
@@ -93,7 +93,7 @@
         </div>
     </section>
 
-<!-- Start of database connection and editing for login --> 
+<!-- LOGIN PHP -->
 <?php
 if (isset($_POST["testUsername"]) && isset($_POST['testPassword'])) 
 {
@@ -185,11 +185,11 @@ if (isset($_POST["testUsername"]) && isset($_POST['testPassword']))
         </script>
     </section>
 
-<!-- Start of database connection and editing for signup --> 
+<!-- SIGNUP PHP -->
 <?php
-if (isset($_POST["newUsername"]) && isset($_POST['newPassword']) && isset($_POST['newBirthdate']) && isset($_POST['newEmail'])) 
+if (isset($_POST["signupUsername"]) && isset($_POST['signupPassword']) && isset($_POST['signupBirthdate']) && isset($_POST['signupEmail'])) 
 {
-	$username = $_POST["newUsername"]; // variable username is the value in the username text box
+	$username = $_POST["signupUsername"]; // variable username is the value in the username text box
 	if ($username == ""){ //check if username has been entered
 		?>
 		<script> alert ("Please enter username.")
@@ -198,109 +198,109 @@ if (isset($_POST["newUsername"]) && isset($_POST['newPassword']) && isset($_POST
 		<?php
 	}
 	else{
-	setcookie("username",$username); // set cookie so username can be used later
-	$email = $_POST['newEmail'];
-	if ($email == ""){ //check if email has been entered
-		?>
-		<script> alert ("Please enter email.")
-		window.location.href='index.php';
-		</script>
-		<?php
-	}
-	else{
-	if (strpos($email, '@') == false)
-	{//email address given is not valid (doesn't contain @)
-		?>
-		<script> alert ("Must use a valid email address.")</script>
-		<?php
-	}
-	else{
-
-	list($user, $domain) = explode('@', $email); //split email into username and domain
-	if ($domain != 'mtu.edu') //if the domain is not an mtu.edu email, the user will be given a popup warning, which will redirect to signup page when exited out of
-	{
-		?>
-		<script> alert ("Must use a valid MTU address.")</script>
-		<?php
-	}
-	else{ //valid email, can move on
-	$password = $_POST['newPassword'];
-	if ($password == ""){ //check if password has been entered
-		?>
-		<script> alert ("Please enter password.")
-		window.location.href='index.php';
-		</script>
-		<?php
-	}
-	else{
-	$time = strtotime($_POST['newBirthdate']); //convert date in html form to sql date format
-	$birthdate = date('Y-m-d', $time);
-	if ($newBirthdate == ""){ //check if birthdate has been entered
-		?>
-		<script> alert ("Please enter birthdate.")
-		window.location.href='index.php';
-		</script>
-		<?php
-	}
-	else{
-	try{
-		$config = parse_ini_file("ProjectDB.ini"); // find database info in .ini file
- 		$dbh = new PDO($config['dsn'], $config['username'], $config['password']); // create connection to database
-  		$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); // error checking
-		
-		//test to see if username or email are already in use
-		$userResult = $dbh->query("select * from Student where username = '" .$username ."'");
-		$userFound = $userResult->fetch();
-		$emailResult = $dbh->query("select * from Student where email = '" .$email ."'");
-		$emailFound = $emailResult->fetch();
-		if ($userFound) //username is already in use, popup error will be given
-		{
+		setcookie("username",$username); // set cookie so username can be used later
+		$email = $_POST['signupEmail'];
+		if ($email == ""){ //check if email has been entered
 			?>
-			<script> alert ("This username is already in use.  Please choose a new username.")</script>
-			<?php
-		}
-		elseif ($emailFound) //email is already in use, popup error will be given
-		{
-			?>
-			<script> alert ("There is already an account under this email.  If you have forgotten your password, please return to the home page to reset it.")</script>
-			<?php
-
-		}
-		else{ //username and email are new, good to continue
-		$query = "Insert into Student(username, email, password, birthdate) values(:newUser, :newEmail, :newPass, :newBirthdate)"; //query to create new account
-		$step = $dbh->prepare($query); //prepare statement to prevent SQL injection
-		$step->bindParam(':newUser', $username); //bind the parameters of query to form answers
-		$step->bindParam(':newEmail', $email);
-		$step->bindParam(':newPass', $password);
-		$step->bindParam(':newBirthdate', $birthdate);
-
-		if($step->execute()){
-			//If query was successful, shows pop up and then redirected to index.php
-			?>
-			<script> alert ("Account created successfully!")
+			<script> alert ("Please enter email.")
 			window.location.href='index.php';
 			</script>
 			<?php
 		}
 		else{
-			//print error messages if query could not be performed
-			print "The account was not able to be created.  Please try again.";
-			print $e->getMessage();	
-		}
-		}	
+			if (strpos($email, '@') == false)
+			{//email address given is not valid (doesn't contain @)
+				?>
+				<script> alert ("Must use a valid email address.")</script>
+				<?php
+			}
+			else{
+				list($user, $domain) = explode('@', $email); //split email into username and domain
+				if ($domain != 'mtu.edu') //if the domain is not an mtu.edu email, the user will be given a popup warning, which will redirect to signup page when exited out of
+				{
+					?>
+					<script> alert ("Must use a valid MTU address.")</script>
+					<?php
+				}
+				else{ //valid email, can move on
+					$password = $_POST['signupPassword'];
+					if ($password == ""){ //check if password has been entered
+						?>
+						<script> alert ("Please enter password.")
+						window.location.href='index.php';
+						</script>
+						<?php
+					}
+					else{
+						$time = strtotime($_POST['signupBirthdate']); //convert date in html form to sql date format
+						$birthdate = date('Y-m-d', $time);
+						if ($time == ""){ //check if birthdate has been entered
+							?>
+							<script> alert ("Please enter birthdate.")
+							window.location.href='index.php';
+							</script>
+							<?php
+						}
+						else{
+							try{
+								$config = parse_ini_file("ProjectDB.ini"); // find database info in .ini file
+ 								$dbh = new PDO($config['dsn'], $config['username'], $config['password']); // create connection to database
+  								$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); // error checking
 		
-	
-	} catch (PDOException $e) {
-		//print error messages if query could not be performed due to backend/database issues
-		print "The account was not able to be created.  Please try again.";
-		print $e->getMessage();
-		die();
+								//test to see if username or email are already in use
+								$userResult = $dbh->query("select * from Student where username = '" .$username ."'");
+								$userFound = $userResult->fetch();
+								$emailResult = $dbh->query("select * from Student where email = '" .$email ."'");
+								$emailFound = $emailResult->fetch();
+								if ($userFound) //username is already in use, popup error will be given
+								{
+									?>
+									<script> alert ("This username is already in use.  Please choose a new username.")</script>
+									<?php
+								}
+								elseif ($emailFound) //email is already in use, popup error will be given
+								{
+									?>
+									<script> alert ("There is already an account under this email.  If you have forgotten your password, please return to the home page to reset it.")</script>
+									<?php
+								}
+								else{ //username and email are new, good to continue
+									$query = "Insert into Student(username, email, password, birthdate) values(:newUser, :newEmail, :newPass, :newBirthdate)"; //query to create new account
+									$step = $dbh->prepare($query); //prepare statement to prevent SQL injection
+									$step->bindParam(':newUser', $username); //bind the parameters of query to form answers
+									$step->bindParam(':newEmail', $email);
+									$step->bindParam(':newPass', $password);
+									$step->bindParam(':newBirthdate', $birthdate);
+									if($step->execute()){
+										//If query was successful, shows pop up and then redirected to index.php
+										?>
+										<script> alert ("Account created successfully!")
+										window.location.href='index.php';
+										</script>
+										<?php
+									}
+									else{
+										//print error messages if query could not be performed
+										print "The account was not able to be created.  Please try again.";
+										print $e->getMessage();	
+									}
+								}	
+							} 
+							catch (PDOException $e) 
+							{
+								//print error messages if query could not be performed due to backend/database issues
+								print "The account was not able to be created.  Please try again.";
+								print $e->getMessage();
+								die();
+							}
+						}
+					}
+				}
+			}
+		}
 	}
-	}
-	}}}}}
 }
 ?>
-
     <!-- Signup Button Section -->
     <section>
         <div id="id02" class="modal">
@@ -310,19 +310,19 @@ if (isset($_POST["newUsername"]) && isset($_POST['newPassword']) && isset($_POST
                 <div class="form">
                     <form method = "post" >
                         <div class="newUsername">
-                            <input type="text" placeholder="Username">
+                            <input type="text" placeholder="Username" name = "signupUsername">
                         </div>
 
                         <div class="newEmail">
-                            <input type="text" placeholder="Email">
+                            <input type="text" placeholder="Email" name = "signupEmail">
                         </div>
 
                         <div class="newBirthdate">
-                            <input placeholder = "Birthdate" class = "textbox-n" type = "text" onfocus = "(this.type = 'date')"  id = "date">
+                            <input placeholder = "Birthdate" class = "textbox-n" type = "text" onfocus = "(this.type = 'date')"  id = "date" name = "signupBirthdate">
                         </div>
 
                         <div class="newPassword">
-                            <input type="password" placeholder="Password">
+                            <input type="password" placeholder="Password" name = "signupPassword">
                         </div>
 
                         <input type = "submit" name = "ok" value = "Create Account" class ="signin-btn">
