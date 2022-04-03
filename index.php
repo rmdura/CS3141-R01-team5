a422fdce5a39100323dc5e4ae9421092c0fa89d5
rmdura <rmdura@mtu.edu>
2022-04-03 18:01:08 -0400

    Security Updates to index.php and newPassword.php
    Password hashing was implemented. Passwords were required to have an uppercase and lowercase letter, a number, a special character, and at least 8 total characters. User birthday's were checked to make sure that users are 18 or older. Prepare statements were added to avoid SQL injection.  Cookie setting was put at top of index.php to resolve warning being thrown.  Else statements were changed into elseif statements for cleaner formatting.  </div> bracket location was changed in signup html section to resolve error of "create account" button not being visible until scrolled over.

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
+<?php
+// must define cookies before any html (throws warning otherwise)
+if (isset($_POST["testUsername"]) && isset($_POST['testPassword'])) 
+{
+	$username = $_POST["testUsername"]; // variable username is the value in the username text box
+	setcookie("username",$username); // set cookie so username can be used later
+}
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -99,17 +108,20 @@
 <?php
 if (isset($_POST["testUsername"]) && isset($_POST['testPassword'])) 
 {
-	$username = $_POST["testUsername"]; // variable username is the value in the username text box
-	setcookie("username",$username); // set cookie so username can be used later
-	$password = $_POST['testPassword'];
+	$password = sha1($_POST['testPassword']); // hashes using sha1 algorithm
 	try{
 		$config = parse_ini_file("ProjectDB.ini"); // find database info in .ini file
  		$dbh = new PDO($config['dsn'], $config['username'], $config['password']); // create connection to database
   		$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); // error checking
 	
-		$result = $dbh->query("select * from Student where username = '" .$username ."' and password = '" .$password ."'"); //look for Student in database with username and password
-		$found = $result->fetch(); //get result of query above
-  		if (! $found) //result did not return an account
+		$loginquery = "Select * from Student where username = :userVal and password = :passVal";
+		$loginstep = $dbh->prepare($loginquery);
+		$loginstep->bindParam(':userVal', $username);
+		$loginstep->bindParam(':passVal', $password);
+		$loginstep->execute();
+		$loginfound = $loginstep->fetch();
+
+  		if (! $loginfound) //result did not return an account
    		{
 			//script will produce popup, which shows the message and returns to index.php when the popup is exited
 			?>
@@ -192,6 +204,30 @@ if (isset($_POST["testUsername"]) && isset($_POST['testPassword']))
 if (isset($_POST["signupUsername"]) && isset($_POST['signupPassword']) && isset($_POST['signupBirthdate']) && isset($_POST['signupEmail'])) 
 {
 	$username = $_POST["signupUsername"]; // variable username is the value in the username text box
+	$email = $_POST['signupEmail'];
+	list($user, $domain) = explode('@', $email); //split email into username and domain
+	$password = $_POST['signupPassword'];
+	$hashed_password = sha1($password); // use sha1 hashing algorithm
+
+	// password requirement variables
+	$uppercase = preg_match('@[A-Z]@', $password); // check for uppercase letter
+	$lowercase = preg_match('@[a-z]@', $password); // check for lowercase letter
+	$special = preg_match('@[^\w]@', $password); // check for special character
+	$number = preg_match('@[0-9]@', $password); // check for number
+
+	$time = strtotime($_POST['signupBirthdate']); //convert date in html form to sql date format
+	$birthdate = date('Y-m-d', $time);
+	
+	//function to find users age from birthday (will use to check if 18)
+	function getAge($bday)
+	{
+		$dob = new DateTime($bday);
+		$now = new DateTime();
+		$diff = $now->diff($dob);
+		$age = $diff->y;
+		return $age;
+	}
+
 	if ($username == ""){ //check if username has been entered
 		?>
 		<script> alert ("Please enter username.")
@@ -199,109 +235,114 @@ if (isset($_POST["signupUsername"]) && isset($_POST['signupPassword']) && isset(
 		</script>
 		<?php
 	}
+	elseif ($email == ""){ //check if email has been entered
+		?>
+		<script> alert ("Please enter email.")
+		window.location.href='index.php';
+		</script>
+		<?php
+	}
+	elseif (strpos($email, '@') == false) {//email address given is not valid (doesn't contain @)
+		?>
+		<script> alert ("Must use a valid email address.")</script>
+		<?php
+	}
+	elseif ($domain != 'mtu.edu'){	//if the domain is not an mtu.edu email, the user will be given a popup warning, which will redirect to signup page when exited out of
+		?>
+		<script> alert ("Must use a valid MTU address.")</script>
+		<?php
+	}
+	elseif ($password == ""){ //check if password has been entered
+		?>
+		<script> alert ("Please enter password.")
+		window.location.href='index.php';
+		</script>
+		<?php
+	}
+	elseif (!$uppercase || !$lowercase || !$special || !$number || strlen($password) < 8){ // check for uppercase, lowercase, special character, number, and 8 or more characters
+		?>
+		<script> alert ("Invalid password.  Your password must be 8 characters long, including an uppercase and lowercase letter, as well as a special character and number.")
+		</script>
+		<?php	
+	}
+	elseif ($time == ""){ //check if birthdate has been entered
+		?>
+		<script> alert ("Please enter birthdate.")
+		window.location.href='index.php';
+		</script>
+		<?php
+	}
+	elseif (getAge($birthdate) < 18){
+		?>
+		<script> alert ("You must be 18 years of age or older to create an account.")
+		window.location.href='index.php';
+		</script>
+		<?php
+	}
 	else{
-		setcookie("username",$username); // set cookie so username can be used later
-		$email = $_POST['signupEmail'];
-		if ($email == ""){ //check if email has been entered
-			?>
-			<script> alert ("Please enter email.")
-			window.location.href='index.php';
-			</script>
-			<?php
-		}
-		else{
-			if (strpos($email, '@') == false)
-			{//email address given is not valid (doesn't contain @)
+		try{
+			$config = parse_ini_file("ProjectDB.ini"); // find database info in .ini file
+ 			$dbh = new PDO($config['dsn'], $config['username'], $config['password']); // create connection to database
+  			$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); // error checking
+		
+			// test to see if username or email are already in use
+			// prepare statements used to prevent SQL injection 
+			$userquery = "Select * from Student where username = :userVal";
+			$userstep = $dbh->prepare($userquery);
+			$userstep->bindParam(':userVal', $username);
+			$userstep->execute();
+			$userFound = $userstep->fetch();
+
+			$emailquery = "Select * from Student where email = :emailVal";
+			$emailstep = $dbh->prepare($emailquery);
+			$emailstep->bindParam(':emailVal', $email);
+			$emailstep->execute();
+			$emailFound = $emailstep->fetch();		
+
+			if ($userFound) //username is already in use, popup error will be given
+			{
 				?>
-				<script> alert ("Must use a valid email address.")</script>
+				<script> alert ("This username is already in use.  Please choose a new username.")</script>
 				<?php
 			}
-			else{
-				list($user, $domain) = explode('@', $email); //split email into username and domain
-				if ($domain != 'mtu.edu') //if the domain is not an mtu.edu email, the user will be given a popup warning, which will redirect to signup page when exited out of
-				{
+			elseif ($emailFound) //email is already in use, popup error will be given
+			{
+				?>
+				<script> alert ("There is already an account under this email.  If you have forgotten your password, please return to the home page to reset it.")</script>
+				<?php
+			}
+			else{ //username and email are new, good to continue
+				$query = "Insert into Student(username, email, password, birthdate) values(:newUser, :newEmail, :newPass, :newBirthdate)"; //query to create new account
+				$step = $dbh->prepare($query); //prepare statement to prevent SQL injection
+				$step->bindParam(':newUser', $username); //bind the parameters of query to form answers
+				$step->bindParam(':newEmail', $email);
+				$step->bindParam(':newPass', $hashed_password);
+				$step->bindParam(':newBirthdate', $birthdate);
+				if($step->execute()){
+					//If query was successful, shows pop up and then redirected to index.php
 					?>
-					<script> alert ("Must use a valid MTU address.")</script>
+					<script> alert ("Account created successfully!")
+					window.location.href='index.php';
+					</script>
 					<?php
 				}
-				else{ //valid email, can move on
-					$password = $_POST['signupPassword'];
-					if ($password == ""){ //check if password has been entered
-						?>
-						<script> alert ("Please enter password.")
-						window.location.href='index.php';
-						</script>
-						<?php
-					}
-					else{
-						$time = strtotime($_POST['signupBirthdate']); //convert date in html form to sql date format
-						$birthdate = date('Y-m-d', $time);
-						if ($time == ""){ //check if birthdate has been entered
-							?>
-							<script> alert ("Please enter birthdate.")
-							window.location.href='index.php';
-							</script>
-							<?php
-						}
-						else{
-							try{
-								$config = parse_ini_file("ProjectDB.ini"); // find database info in .ini file
- 								$dbh = new PDO($config['dsn'], $config['username'], $config['password']); // create connection to database
-  								$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); // error checking
-		
-								//test to see if username or email are already in use
-								$userResult = $dbh->query("select * from Student where username = '" .$username ."'");
-								$userFound = $userResult->fetch();
-								$emailResult = $dbh->query("select * from Student where email = '" .$email ."'");
-								$emailFound = $emailResult->fetch();
-								if ($userFound) //username is already in use, popup error will be given
-								{
-									?>
-									<script> alert ("This username is already in use.  Please choose a new username.")</script>
-									<?php
-								}
-								elseif ($emailFound) //email is already in use, popup error will be given
-								{
-									?>
-									<script> alert ("There is already an account under this email.  If you have forgotten your password, please return to the home page to reset it.")</script>
-									<?php
-								}
-								else{ //username and email are new, good to continue
-									$query = "Insert into Student(username, email, password, birthdate) values(:newUser, :newEmail, :newPass, :newBirthdate)"; //query to create new account
-									$step = $dbh->prepare($query); //prepare statement to prevent SQL injection
-									$step->bindParam(':newUser', $username); //bind the parameters of query to form answers
-									$step->bindParam(':newEmail', $email);
-									$step->bindParam(':newPass', $password);
-									$step->bindParam(':newBirthdate', $birthdate);
-									if($step->execute()){
-										//If query was successful, shows pop up and then redirected to index.php
-										?>
-										<script> alert ("Account created successfully!")
-										window.location.href='index.php';
-										</script>
-										<?php
-									}
-									else{
-										//print error messages if query could not be performed
-										print "The account was not able to be created.  Please try again.";
-										print $e->getMessage();	
-									}
-								}	
-							} 
-							catch (PDOException $e) 
-							{
-								//print error messages if query could not be performed due to backend/database issues
-								print "The account was not able to be created.  Please try again.";
-								print $e->getMessage();
-								die();
-							}
-						}
-					}
+				else{
+					//print error messages if query could not be performed
+					print "The account was not able to be created.  Please try again.";
+					print $e->getMessage();	
 				}
-			}
+			}	
+		} 
+		catch (PDOException $e) 
+		{
+			//print error messages if query could not be performed due to backend/database issues
+			print "The account was not able to be created.  Please try again.";
+			print $e->getMessage();
+			die();
 		}
 	}
 }
+
 ?>
     <!-- Signup Button Section -->
     <section>
@@ -326,12 +367,12 @@ if (isset($_POST["signupUsername"]) && isset($_POST['signupPassword']) && isset(
                         <div class="newPassword">
                             <input type="password" placeholder="Password" name = "signupPassword">
                         </div>
-
+		</div>
                         <input type = "submit" name = "ok" value = "Create Account" class ="signin-btn">
                     </form>
 
                     <button type="button" onclick="document.getElementById('id02').style.display='none'" class="cancel-btn">Cancel</button>
-                </div>
+                
             </div>
         </div>
     </section>
--- newPassword.php
+++ newPassword.php
@@ -9,7 +9,7 @@
 <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;300;400;600;700&display=swap" rel="stylesheet">
 </head>
 
-<header>Forgor Password Page</header>
+<header>Forgot Password Page</header>
 
 <body>
 
@@ -21,16 +21,29 @@ if (isset($_POST["oldUsername"]) && isset($_POST['newPassword']) && isset($_POST
 	$username = $_POST["oldUsername"]; // variable username is the value in the username text box
 	$email = $_POST['oldEmail'];
 	$password = $_POST['newPassword'];
+	$hashed_password = sha1($password); // use sha1 hashing algorithm
 	$time = strtotime($_POST['oldBirthdate']); //convert date in html form to sql date format
 	$birthdate = date('Y-m-d', $time);
+	
+	// password requirement variables
+	$uppercase = preg_match('@[A-Z]@', $password); // check for uppercase letter
+	$lowercase = preg_match('@[a-z]@', $password); // check for lowercase letter
+	$special = preg_match('@[^\w]@', $password); // check for special character
+	$number = preg_match('@[0-9]@', $password); // check for number
+
 	try{
 		$config = parse_ini_file("ProjectDB.ini"); // find database info in .ini file
  		$dbh = new PDO($config['dsn'], $config['username'], $config['password']); // create connection to database
   		$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); // error checking
 		
-		//query to look for account with the credentials given
-		$accountResult = $dbh->query("select * from Student where username = '" .$username ."' and email = '" .$email ."' and birthdate = '" .$birthdate ."'");
-		$accountFound = $accountResult->fetch();
+		//query to look for account with the credentials given (prepare used to prevent SQL injection)
+		$accountquery = "Select * from Student where username = :userVal and email = :emailVal and birthdate = :birthVal";
+		$accountstep = $dbh->prepare($accountquery);
+		$accountstep->bindParam(':userVal', $username);
+		$accountstep->bindParam(':emailVal', $email);
+		$accountstep->bindParam(':birthVal', $birthdate);
+		$accountstep->execute();
+		$accountFound = $accountstep->fetch();
 		if (! $accountFound) //result did not return an account
    		{
 			//script will produce popup saying that credentials aren't linked to current account
@@ -38,11 +51,18 @@ if (isset($_POST["oldUsername"]) && isset($_POST['newPassword']) && isset($_POST
 			<script> alert ("The credentials given are not linked to a current account.  Please make sure that the username, email, and birthdate are correct and try again.")</script>
 			<?php
   		}
+		else if (!$uppercase || !$lowercase || !$special || !$number || strlen($password) < 8) // check for uppercase, lowercase, special character, number, and 8 or more characters
+		{
+			?>
+			<script> alert ("Invalid password.  Your password must be 8 characters long, including an uppercase and lowercase letter, as well as a special character and number.")
+			</script>
+			<?php	
+		}
 		else{ //good to change password
 			$query = "Update Student set password = :newPassword where username = :oldUsername"; //query to update password
 			$step = $dbh->prepare($query); //prepare statement to prevent SQL injection
 			$step->bindParam(':oldUsername', $username); //bind the parameters of query to form answers
-			$step->bindParam(':newPassword', $password);
+			$step->bindParam(':newPassword', $hashed_password);
 			if($step->execute()){
 				//if successful, shows popup and redirects to index.php
 				?>
