53299366bb36612e76df67fbe01aae33baf99779
Scorekeeper2883 <pjellens@mtu.edu>
2022-04-04 12:53:23 -0400

    Update DashBoard.php

--- DashBoard.php
+++ DashBoard.php
@@ -1,17 +1,33 @@
+<!DOCTYPE html>
 <html>
-	<head>
-	    <meta name="viewport" content="with=device-width, initial-scale=1.0">
-	    <title>MTU Student Socializing Platform</title>
-	    <link rel="stylesheet" href="style.css">
-	    <link rel="preconnect" href="https://fonts.googleapis.com">
-		<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-		<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;300;400;600;700&display=swap" rel="stylesheet">
-	</head>
+<head>
+    <meta name="viewport" content="with=device-width, initial-scale=1.0">
+    <title>MTU Student Socializing Platform</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;300;400;600;700&display=swap" rel="stylesheet">
+</head>
+
+<body>
+	<section class="header">
+	    <nav>
+	    	// Uses the logo to lead to DashBoard since they are logged in.
+	        <a href="DashBoard.php"><img src="images/logo.png"></a>
+	    </nav>
+	    
+	</section>
 	
-	<body>
-    <section class="header">
-        <nav>
-            <a href="index.php"><img src="images/logo.png"></a>
-        </nav>
-	</body>
+    <?php
+    	try {
+			$config = parse_ini_file("ProjectDB.ini"); // find database info in .ini file
+			$dbh = new PDO($config['dsn'], $config['username'], $config['password']); // create connection to database
+			$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); // error checking
+		} catch (PDOException $e) {
+			//print error messages if query could not be performed due to backend/database issues
+			print $e->getMessage();
+			die();
+		}
+	?>
+</body>
 </html>
