f659f16778fd17f4c565a26d92430195d965e228
Scorekeeper2883 <pjellens@mtu.edu>
2022-04-04 13:29:56 -0400

    Update DashBoard.php

--- DashBoard.php
+++ DashBoard.php
@@ -1,23 +1,20 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <meta name="viewport" content="with=device-width, initial-scale=1.0">
+    <meta name = "viewport" content="with=device-width, initial-scale=1.0">
     <title>MTU Student Socializing Platform</title>
-    <link rel="stylesheet" href="style.css">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-	<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;300;400;600;700&display=swap" rel="stylesheet">
+    <link rel = "stylesheet" href="style.css">
+    <link rel = "preconnect" href="https://fonts.googleapis.com">
+	<link rel = "preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href = "https://fonts.googleapis.com/css2?family=Poppins:wght@100;300;400;600;700&display=swap" rel="stylesheet">
 </head>
-
 <body>
 	<section class="header">
 	    <nav>
-	    	// Uses the logo to lead to DashBoard since they are logged in.
-	        <a href="DashBoard.php"><img src="images/logo.png"></a>
+	    	// Uses the logo to lead to their DashBoard since they are logged in.
+	        <a href = "DashBoard.php"><img src="images/logo.png"></a>
 	    </nav>
-	    
 	</section>
-	
     <?php
     	try {
 			$config = parse_ini_file("ProjectDB.ini"); // find database info in .ini file
@@ -29,5 +26,33 @@
 			die();
 		}
 	?>
+	// Creates a table with 5 columns on the website.
+	<table>
+		<tr>
+			<th>Event</th>
+			<th>Time</th>
+			<th>Date</th>
+			<th>Location</th>
+			<th>Description</th>
+		</tr>
+		<?php
+			// Pulls data for the table above from the database.
+			$sql = "SELECT name, event_time, event_date, location, description from Event";
+			$result = $dbh-> query($sql);
+			
+			// Checks if there are any results.
+			if($result-> num_rows > 0) {
+				while($row = $result-> fetch_assoc()) {
+					echo "<tr><td>". $row["Event"]."</td><td>". $row["Time"]."</td><td>". $row["Date"]."</td><td>". $row["Location"]."</td><td>". $row["Description"]."</td></tr>";
+				}
+			echo "</table";
+			} else {
+				echo "0 results";
+			}
+			
+			// Closes connection to the database.
+			$dbh-> close();
+		?>
+	</table>
 </body>
 </html>
