4910ec8b59ac6ea665a21376349db15220715f5e
Scorekeeper2883 <pjellens@mtu.edu>
2022-04-11 17:38:32 -0400

    Update DashBoard.php
    Finally pulls all events that the user has joined or created.

--- DashBoard.php
+++ DashBoard.php
@@ -23,28 +23,43 @@
 		}
 	?>
 	<!--Creates a table with 5 columns on the website.-->
-	<?php
-		echo "<table border = '1' width = '300' cellspacing = '0'>";
-			echo "<tr bgcolor = 'yellow'>";
-				echo "<td>Event</td>";
-				echo "<td>Time</td>";
-				echo "<td>Date</td>";
-				echo "<td>Location</td>";
-				echo "<td>Description</td>";
-			echo "</tr>";
+	<table border='1' width='1200' style='text-align:center' cellspacing='0'>
+		<tr>
+			<th>Event</th>
+			<th>Time</th>
+			<th>Date</th>
+			<th>Location</th>
+			<th>Description</th>
+		</tr>
 		
-		// Pulls data for the table above from the database.
-		$sql = "SELECT name, event_time, event_date, location, description FROM Event 
-			WHERE Event.event_index=(SELECT event_id FROM Student_Event WHERE student_name='testUser')";
-		$result = $dbh->query($sql);
-		
-		// Checks if there are any results.
-		if($result->rowCount() > 0) {
-			while($row = $result->fetch()) {
-				echo "<tr><td>". $row["name"]."</td><td>". $row["event_time"]."</td><td>". $row["event_date"]."</td><td>". 
-				$row["location"]."</td><td>". $row["description"]."</td></tr>";
+		<?php
+			$user='pjellens';
+			
+			// Pulls joined events data for the table above from the database.
+			$sql = "SELECT name, event_time, event_date, location, description FROM Event 
+				WHERE event_index IN (SELECT event_id FROM Student_Event WHERE student_name='$user')";
+			$result = $dbh->query($sql);
+			
+			// Checks if there are any results.
+			if($result->rowCount() > 0) {
+				while($row = $result->fetch()) {
+					echo "<tr><td>". $row["name"]."</td><td>". $row["event_time"]."</td><td>". $row["event_date"]."</td><td>". 
+					$row["location"]."</td><td>". $row["description"]."</td></tr>";
+				}
 			}
-		}
-	?>
+			
+			// Pulls owned events data for the table above from the database.
+			$sql = "SELECT name, event_time, event_date, location, description FROM Event WHERE owner='$user'";
+			$result = $dbh->query($sql);
+			
+			// Checks if there are any results.
+			if($result->rowCount() > 0) {
+				while($row = $result->fetch()) {
+					echo "<tr><td>". $row["name"]."</td><td>". $row["event_time"]."</td><td>". $row["event_date"]."</td><td>". 
+					$row["location"]."</td><td>". $row["description"]."</td></tr>";
+				}
+			}
+		?>
+	</table>
 </body>
 </html>
