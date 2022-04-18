ef3d82ad8790c55e9d9ff95ca23a27936bb4f9b6
Eli Pinnoo <empinnoo@mtu.edu>
2022-04-18 19:57:23 -0400

    Update FindEventsdb.php
    Trying to grab the tag the person searched for

--- FindEventsdb.php
+++ FindEventsdb.php
@@ -6,8 +6,25 @@
 $config = parse_ini_file("ProjectDB.ini"); // find database info in .ini file
 $dbh = new PDO($config['dsn'], $config['username'], $config['password']); // create connection to database
 $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); // error checking
-  
-// SQL query to select data from database
-$eventQuery = $dbh->query("SELECT event_index, description, event_date, event_time, location, name FROM Event");
+
+// Declaring some variables;
+$look = "";
+
+// Detect if the search button is clicked
+if (isset($_POST["Submit"]))
+{
+    $look = $_GET["Submit"];
+}
+
+// SQL query with given input
+if (!($look == ""))
+{
+    // put: eventquery = dbh->queary(......)
+}
+// Default SQL query to select data from database
+else
+{
+    $eventQuery = $dbh->query("SELECT event_index, description, event_date, event_time, location, name FROM Event");
+}
 
 ?>
