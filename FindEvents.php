2ada8ae2d5d3e63b1caa734ccfe83adc0095ace1
Eli Pinnoo <empinnoo@mtu.edu>
2022-04-19 22:29:20 -0400

    search bar
    for josh

--- FindEvents.php
+++ FindEvents.php
@@ -6,26 +6,27 @@ include 'FindEventsdb.php';
 <!-- HTML code to display -->
 <!DOCTYPE html>
 <html>
+
 <head>
     <meta name="viewport" content="with=device-width, initial-scale=1.0">
-	<meta charset="utf-8" />
+    <meta charset="utf-8" />
     <title>MTU Student Socializing Platform</title>
     <link rel="stylesheet" href="style.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
-	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-	<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;300;400;600;700&display=swap" rel="stylesheet">
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;300;400;600;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
 
     <!-- CSS for styling the table and page -->
     <style>
         body {
-			background-image: url(images/mainWebBG.png);
-			background-repeat: no-repeat;
-			background-attachment: fixed;
-			background-position: center;
-			background-size: cover;
-		}
+            background-image: url(images/mainWebBG.png);
+            background-repeat: no-repeat;
+            background-attachment: fixed;
+            background-position: center;
+            background-size: cover;
+        }
 
         .findEvents {
             color: #FFFFFF;
@@ -47,20 +48,20 @@ include 'FindEventsdb.php';
             font-size: large;
             border: 1px solid black;
         }
-        
+
         h1 {
             padding: 20px;
             text-align: center;
             color: #FFFFFF;
             font-size: xx-large;
-            font-family: 'Gill Sans', 'Gill Sans MT', 
-            ' Calibri', 'Trebuchet MS', 'sans-serif';
+            font-family: 'Gill Sans', 'Gill Sans MT',
+                ' Calibri', 'Trebuchet MS', 'sans-serif';
         }
-  
+
         td {
             border: 1px solid black;
         }
-  
+
         th,
         td {
             color: #FFFFFF;
@@ -70,7 +71,7 @@ include 'FindEventsdb.php';
             padding: 10px;
             text-align: center;
         }
-  
+
         td {
             font-weight: lighter;
         }
@@ -83,57 +84,58 @@ session_start();
 ?>
 
 <body>
-<div class="bodyInfo">
-<?php include 'LeftFloatingNavBar.html'; ?>
+    <div class="bodyInfo">
+        <?php include 'LeftFloatingNavBar.html'; ?>
 
-    <h3 class="findEvents">Find Events Page</h3>
+        <h3 class="findEvents">Find Events Page</h3>
 
-    <!-- Creating search bar and it's button -->
-    <div class="dropdown">
-		Find Event: <input type="text" name="newEventTag" class="form-control form-control-lg" placeholder="Type Here..." id="newEventTag" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" onkeyup="javascript:load_data(this.value)"/>
-		<span id="search_result"></span>
-	</div>
-    <div class="searchButton">
-        <input type = "submit" name = "Submit" id="btn" value = "Search Events">
-    </div>
+        <form aciton=FindEventsdb.php method=post>
+            <!-- Creating search bar and it's button -->
+            <div class="dropdown">
+                Find Event: <input type="text" name="newEventTag" class="form-control form-control-lg" placeholder="Type Here..." id="findEventString" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" onkeyup="javascript:load_data(this.value)" />
+                <span id="search_result"></span>
+            </div>
+            <div class="searchButton">
+                <input type="submit" name="Submit" id="btn" value="Search Events">
+            </div>
+        </form>
 
-    <!-- Adding table with event information -->
-    <section>
-        <h1>Event information</h1>
+        <!-- Adding table with event information -->
+        <section>
+            <h1>Event information</h1>
 
-        <!-- TABLE CONSTRUCTION-->
-        <table>
-            <tr>
-		        <th>name:</th>
-                <th>description:</th>
-                <th>event date:</th>
-                <th>event time:</th>
-                <th>location:</th>
-                <th>join event:</th>
-            </tr>
-
-            <!-- PHP CODE TO FETCH DATA FROM ROWS-->
-            <?php   // LOOP TILL END OF DATA 
-                while($rows=$eventQuery->fetch())
-                {
-             ?>
-            <form action=JoinEvent.php method=post>
+            <!-- TABLE CONSTRUCTION-->
+            <table>
                 <tr>
-                    <!--FETCHING DATA FROM EACH ROW OF EVERY COLUMN-->
-		            <td><?php echo $rows['name'];?></td>
-                    <td><?php echo $rows['description'];?></td>
-                    <td><?php echo $rows['event_date'];?></td>
-                    <td><?php echo $rows['event_time'];?></td>
-                    <td><?php echo $rows['location'];?></td>
-                    <td><button type="submit" name="join" value=<?php print_r($rows['event_index']);?>>Join Event</button></td>
+                    <th>name:</th>
+                    <th>description:</th>
+                    <th>event date:</th>
+                    <th>event time:</th>
+                    <th>location:</th>
+                    <th>join event:</th>
                 </tr>
-            </form>
-            <?php
+
+                <!-- PHP CODE TO FETCH DATA FROM ROWS-->
+                <?php   // LOOP TILL END OF DATA 
+                while ($rows = $eventQuery->fetch()) {
+                ?>
+                    <form action=JoinEvent.php method=post>
+                        <tr>
+                            <!--FETCHING DATA FROM EACH ROW OF EVERY COLUMN-->
+                            <td><?php echo $rows['name']; ?></td>
+                            <td><?php echo $rows['description']; ?></td>
+                            <td><?php echo $rows['event_date']; ?></td>
+                            <td><?php echo $rows['event_time']; ?></td>
+                            <td><?php echo $rows['location']; ?></td>
+                            <td><button type="submit" name="join" value=<?php print_r($rows['event_index']); ?>>Join Event</button></td>
+                        </tr>
+                    </form>
+                <?php
                 }
-             ?>
-        </table>
-    </section>
-</div>
+                ?>
+            </table>
+        </section>
+    </div>
 </body>
 
 </html>
--- FindEventsdb.php
+++ FindEventsdb.php
@@ -7,24 +7,22 @@ $config = parse_ini_file("ProjectDB.ini"); // find database info in .ini file
 $dbh = new PDO($config['dsn'], $config['username'], $config['password']); // create connection to database
 $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); // error checking
 
-// Declaring some variables;
-$look = "";
-
-// Detect if the search button is clicked
-if (isset($_POST["Submit"]))
-{
-    $look = $_GET["Submit"];
-}
+// Declaring necessary variables
+$user = $_COOKIE['username'];
 
 // SQL query with given input
-if (!($look == ""))
+if (isset($_POST["btn"]) && !empty(($_POST["findEventString"])))
 {
-    // put: eventquery = dbh->queary(......)
+    $eventString = $_POST["findEventString"];
+    $condition = preg_replace('/[^A-Za-z0-9\- ]/', '', $_POST["query"]);
+    $eventQuery = $dbh->query("SELECT event_index, name, event_time, event_date, location, description FROM Event 
+    WHERE event_index NOT IN (SELECT event_id FROM Student_Event WHERE student_name='$user') AND event_index NOT IN (SELECT event_index FROM Event WHERE owner='$user') AND name LIKE '%".$condition."%'");
 }
 // Default SQL query to select data from database
 else
 {
-    $eventQuery = $dbh->query("SELECT event_index, description, event_date, event_time, location, name FROM Event");
+    $eventQuery = $dbh->query("SELECT event_index, name, event_time, event_date, location, description FROM Event 
+    WHERE event_index NOT IN (SELECT event_id FROM Student_Event WHERE student_name='$user') AND event_index NOT IN (SELECT event_index FROM Event WHERE owner='$user')");
 }
 
 ?>
